12bab1519fd2a0c05c617668e7621298bd9f26f7
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-05-03 11:23:38 +0200

    Refactored player shoot bullet code into a separate function

--- src/systems/player_controls.rs
+++ src/systems/player_controls.rs
@@ -68,7 +68,7 @@ impl<'a> System<'a> for PlayerControlsSystem {
             .map(|goal| goal.next_level)
             .unwrap_or(false);
 
-        let mut shoot_bullet = false;
+        let mut should_shoot_bullet = false;
 
         for (
             player,
@@ -132,7 +132,7 @@ impl<'a> System<'a> for PlayerControlsSystem {
                     &sides_touching,
                 );
 
-                shoot_bullet =
+                should_shoot_bullet =
                     handle_attack(&input_manager, player, animations_container)
                         && player.items_data.can_shoot;
 
@@ -163,69 +163,22 @@ impl<'a> System<'a> for PlayerControlsSystem {
         }
 
         // Player BulletShoot
-        if shoot_bullet {
-            if let Some((player, player_transform, player_flipped)) =
-                (&players, &transforms, &flippeds).join().next()
-            {
-                let spritesheet_handle = spritesheet_handles
-                    .get("player_bullets")
-                    .expect("'player_bullets' spritesheet does not exist");
-                let entity = entities.create();
-                bullets
-                    .insert(
-                        entity,
-                        Bullet::new()
-                            .owner(BulletOwner::Player)
-                            .damage(player.items_data.bullet_damage)
-                            .lifetime(player.items_data.bullet_lifetime)
-                            .build(),
-                    )
-                    .unwrap();
-                collisions.insert(entity, Collision::new()).unwrap();
-                check_collisions.insert(entity, CheckCollision).unwrap();
-                let mut transform = Transform::default();
-                let (x, y, z) = {
-                    let trans = player_transform.translation();
-                    (trans.x, trans.y, trans.z)
-                };
-                transform.set_xyz(x, y, z);
-                transforms.insert(entity, transform);
-                velocities
-                    .insert(
-                        entity,
-                        Velocity::new(
-                            player.items_data.bullet_velocity.0
-                                * match player_flipped {
-                                    Flipped::None => 1.0,
-                                    Flipped::Horizontal => -1.0,
-                                    _ => 1.0,
-                                },
-                            player.items_data.bullet_velocity.1,
-                        ),
-                    )
-                    .unwrap();
-                sizes
-                    .insert(entity, Size::from(player.items_data.bullet_size))
-                    .unwrap();
-                sprite_renders
-                    .insert(entity, SpriteRender {
-                        sprite_sheet:  spritesheet_handle.clone(),
-                        sprite_number: 0,
-                    })
-                    .unwrap();
-                animations
-                    .insert(
-                        entity,
-                        Animation::new()
-                            .default_sprite_sheet_handle(spritesheet_handle)
-                            .default_delay_ms(50)
-                            .sprite_ids(vec![0, 1, 2])
-                            .build(),
-                    )
-                    .unwrap();
-                transparents.insert(entity, Transparent).unwrap();
-                flippeds.insert(entity, player_flipped.clone()).unwrap();
-            }
+        if should_shoot_bullet {
+            shoot_bullet(
+                &entities,
+                &spritesheet_handles,
+                &players,
+                &mut transforms,
+                &mut velocities,
+                &mut sizes,
+                &mut flippeds,
+                &mut bullets,
+                &mut collisions,
+                &mut check_collisions,
+                &mut sprite_renders,
+                &mut animations,
+                &mut transparents,
+            );
         }
     }
 }
@@ -436,5 +389,86 @@ fn handle_item_purchase<'a>(
                 player.take_damage(item.cost);
             }
         }
+    }
+}
+
+fn shoot_bullet<'a>(
+    entities: &Entities<'a>,
+    spritesheet_handles: &SpriteSheetHandles,
+    players: &WriteStorage<'a, Player>,
+    transforms: &mut WriteStorage<'a, Transform>,
+    velocities: &mut WriteStorage<'a, Velocity>,
+    sizes: &mut WriteStorage<'a, Size>,
+    flippeds: &mut WriteStorage<'a, Flipped>,
+    bullets: &mut WriteStorage<'a, Bullet>,
+    collisions: &mut WriteStorage<'a, Collision>,
+    check_collisions: &mut WriteStorage<'a, CheckCollision>,
+    sprite_renders: &mut WriteStorage<'a, SpriteRender>,
+    animations: &mut WriteStorage<'a, Animation>,
+    transparents: &mut WriteStorage<'a, Transparent>,
+) {
+    let player_data_opt = (players, &*transforms, &*flippeds)
+        .join()
+        .next()
+        .map(|(player, transform, flipped)| {
+            let trans = transform.translation();
+            (player, (trans.x, trans.y, trans.z), flipped)
+        });
+
+    if let Some((player, player_pos, player_flipped)) = player_data_opt {
+        let spritesheet_handle = spritesheet_handles
+            .get("player_bullets")
+            .expect("'player_bullets' spritesheet does not exist");
+        let entity = entities.create();
+        bullets
+            .insert(
+                entity,
+                Bullet::new()
+                    .owner(BulletOwner::Player)
+                    .damage(player.items_data.bullet_damage)
+                    .lifetime(player.items_data.bullet_lifetime)
+                    .build(),
+            )
+            .unwrap();
+        collisions.insert(entity, Collision::new()).unwrap();
+        check_collisions.insert(entity, CheckCollision).unwrap();
+        let mut transform = Transform::default();
+        transform.set_xyz(player_pos.0, player_pos.1, player_pos.2);
+        transforms.insert(entity, transform).unwrap();
+        velocities
+            .insert(
+                entity,
+                Velocity::new(
+                    player.items_data.bullet_velocity.0
+                        * match player_flipped {
+                            Flipped::None => 1.0,
+                            Flipped::Horizontal => -1.0,
+                            _ => 1.0,
+                        },
+                    player.items_data.bullet_velocity.1,
+                ),
+            )
+            .unwrap();
+        sizes
+            .insert(entity, Size::from(player.items_data.bullet_size))
+            .unwrap();
+        sprite_renders
+            .insert(entity, SpriteRender {
+                sprite_sheet:  spritesheet_handle.clone(),
+                sprite_number: 0,
+            })
+            .unwrap();
+        animations
+            .insert(
+                entity,
+                Animation::new()
+                    .default_sprite_sheet_handle(spritesheet_handle)
+                    .default_delay_ms(50)
+                    .sprite_ids(vec![0, 1, 2])
+                    .build(),
+            )
+            .unwrap();
+        transparents.insert(entity, Transparent).unwrap();
+        flippeds.insert(entity, player_flipped.clone()).unwrap();
     }
 }
