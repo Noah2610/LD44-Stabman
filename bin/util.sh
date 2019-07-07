1216c5554ec20ae7ecd62c2dcd05aee143e7cb33
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-07-07 08:18:56 +0200

    bin/ scripts: make variables local, error logging, refactoring

--- bin/util.sh
+++ bin/util.sh
@@ -1,17 +1,38 @@
+LOGFILE="$ROOT/logs/$( basename "$0" ).log"
+logfile_dir="$( dirname "$LOGFILE" )"
+! [ -d "$logfile_dir" ] && mkdir -p "$logfile_dir"
+unset logfile_dir
+
 COLOR_ERR="1;31"
 COLOR_MSG="0;33"
 COLOR_MSG_STRONG="1;33"
 COLOR_CODE="0;36;40"
 
+function print_log {
+  local txt="$1"
+  [ -z "$txt" ] && err "No message text given to function \`$0\`"
+  echo -e "$( strip_ansi_codes "$txt" )\n" >> "$LOGFILE"
+}
+
+function strip_ansi_codes {
+  local txt="$1"
+  if is_available "sed"; then
+    echo -e "$txt" | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g"
+  else
+    echo -e "$txt"
+  fi
+}
+
 function colored {
-  color="$1"
-  txt="$2"
+  local color="$1"
+  local txt="$2"
   ([ -z "$color" ] || [ -z "$txt" ]) &&
     err "Function \`$0\` needs two arguments: the color and the text."
   echo "\033[${color}m${txt}\033[m"
 }
 
 function err {
+  print_log "$( semantic_date )\nERROR: $1"
   (1>&2 echo -e "$( colored "$COLOR_ERR" "ERROR:" ) $1\nExiting.")
   exit 1
 }
@@ -24,23 +45,42 @@ function msg_strong {
   echo -e "$( colored "${COLOR_MSG_STRONG}" "${1}" )"
 }
 
+function semantic_date {
+  check "date"
+  local dstr="$( date '+%F %T' )"
+  if is_available "boxed-string"; then
+    BOXED_PADDING_HORZ=1 \
+    BOXED_PADDING_VERT=0 \
+    boxed-string -- "$dstr"
+  else
+    echo "$dstr"
+  fi
+}
+
+function is_available {
+  local cmd="$1"
+  [ -z "$cmd" ] && err "No command to check for availability given to function \`$0\`"
+  command -v "$cmd" &> /dev/null
+}
+
 function check {
-  cmd="$1"
+  local cmd="$1"
   [ -z "$cmd" ] && err "No command to check given to function \`$0\`"
-  command -v "$cmd" &> /dev/null || err "\`$( colored "$COLOR_CODE" "$cmd" )\` is not available."
+  is_available "$cmd" &> /dev/null || err "\`$( colored "$COLOR_CODE" "$cmd" )\` is not available."
 }
 
 function try_run {
-  cmd="$1"
+  local cmd="$1"
   [ -z "$cmd" ] && err "No command given."
+  local out
   if ! out="$( $cmd 2>&1 )"; then
     err "Command failed:\n\033[33m${cmd}\033[m\nReturned:\n${out}"
   fi
 }
 
 function run_terminal {
-  cmd="$1"
-  cmd_bash="bash -c '$cmd || (echo -e \"----------\n[CONTINUE]\"; read')"
+  local cmd="$1"
+  local cmd_bash="bash -c '$cmd || (echo -e \"----------\n[CONTINUE]\"; read')"
   ([ -n "$cmd" ] || err "No command given to function \`$0\`.") &&
     check "cargo" &&
     check "termite" &&
