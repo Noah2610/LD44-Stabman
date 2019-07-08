9fbce264e64d3584f44f997bc4978ed42b331824
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-07-08 23:03:29 +0200

    Update bin/download-util.sh

--- bin/download-util.sh
+++ bin/download-util.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Small script that attempts to download my util.sh helper script.
-# Version: 1.0.1
-GIST_URL="https://gist.githubusercontent.com/Noah2610/68f0351ff2d4970f0403edb03cc5bde6/raw/87be2e6f88f7adae1bedd1ec15fd9da5c69483f5/util.sh"
+# Version: 1.0.2
+URL="https://raw.githubusercontent.com/Noah2610/util.sh/master/util.sh"
 function err { (echo -e "ERROR: $1\nExiting" 1>&2); exit 1; }
 function check { command -v "$1" &> /dev/null || err "'$1' is not available"; }
 check "dirname"; check "curl"
 path="$( dirname "$0" )/util.sh"
-echo -e "Attempting to download \`util.sh\` script from\n  ${GIST_URL}\nto\n  ${path}"
+echo -e "Attempting to download \`util.sh\` script from\n  ${URL}\nto\n  ${path}"
 [ -f "$path" ] && err "File exists at '${path}'"
-out="$( { ( curl "$GIST_URL" ) 1> "$path"; } 2>&1 )" || { rm "$path"; err "$out"; }
+out="$( { ( curl "$URL" ) 1> "$path"; } 2>&1 )" || { rm "$path"; err "$out"; }
