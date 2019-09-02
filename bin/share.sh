65d5cb1799426bf7ec0e1a6a256d8db6470be963
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-09-02 17:17:10 +0200

    Move shared code from bin/run and bin/build to bin/share.sh

--- bin/share.sh
+++ bin/share.sh
@@ -1,5 +1,32 @@
+# shellcheck source=./util.sh source=./share.sh
+_dir="$( dirname "$0" )"
+[ -f "${_dir}/util.sh" ] || bash "${_dir}/download-util.sh" || exit 1
+source "${_dir}/util.sh"
+unset _dir
+
 # https://stackoverflow.com/a/17841619/10927893
 function join_by { local IFS="$1"; shift; echo "$*"; }
+
+function cargo_cmd {
+  local cargo_subcmd="$1"
+  [ -z "$cargo_subcmd" ] && err "No cargo subcommand passed to function \`cargo_cmd\`"
+
+  check "cargo"
+  local args=("$@")
+  unset 'args[0]'
+  local features_str
+  local features=("$RUN_FEATURES")
+  features_str="$( join_by ',' "${features[@]}" )"
+  local cmd="cargo +$RUST_VERSION $cargo_subcmd --features $features_str ${args[*]}"
+  local run_msg
+  run_msg="$( colored "$COLOR_MSG_STRONG" "RUNNING:" ) $( colored "$COLOR_CODE" "$cmd" )"
+  echo -e "$run_msg"
+  if should_run_in_terminal; then
+    run_terminal "$cmd"
+  else
+    $cmd
+  fi
+}
 
 LOGFILE="${ROOT}/logs/$( basename "$0" ).log"
 RUST_VERSION="nightly-2019-03-01"
