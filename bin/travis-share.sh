8e58a867be5ce4d2977ffb329a98eedf74ffecba
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-09-16 18:19:10 +0200

    Add bin/travis-share.sh and bin/travis-test; test built executable with travis

---
+++ bin/travis-share.sh
@@ -0,0 +1,26 @@
+# Shared bash code for travis scripts.
+
+# shellcheck source=./util.sh source=./share.sh
+_dir="$( dirname "$0" )"
+[ -f "${_dir}/util.sh" ] || bash "${_dir}/download-util.sh" || exit 1
+source "${_dir}/util.sh"
+unset _dir
+
+# Only run if this script was started from Travis
+[ -z "$TRAVIS" ] && err "This script should only be run from Travis"
+
+function get_release_path {
+  check "cat"
+  local file_with_path="${ROOT}/.travis-release-path-${TARGET}"
+  check_file "$file_with_path"
+  cat "$file_with_path"
+}
+
+_logdir="${ROOT}/logs"
+[ -d "$_logdir" ] || mkdir -p "$_logdir"
+LOGFILE="${_logdir}/$( basename "$0" ).log"
+unset _logdir
+
+TARGET="$TRAVIS_OS_NAME"
+RELEASE_TARGETS="$TARGET"
+EXE_NAME="$RELEASE_EXE_NAME_OUTPUT"
